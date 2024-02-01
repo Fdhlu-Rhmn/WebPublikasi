ed3272d08b9370ec7a2e9518768104f4b585b278
Fdhlu-Rhmn <fadhlurahmanf10@gmail.com>
2024-02-01 16:40:19 +0800

    memperbaiki storage

--- app/Livewire/Beranda.php
+++ app/Livewire/Beranda.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Books;
 use Livewire\Component;
 
 class Beranda extends Component
 {
+
+    public $books;
+
+    public function mount()
+    {
+        $this->books = Books::all();
+    }
     public function render()
     {
         return view('livewire.beranda');
